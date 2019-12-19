8ef819fe69dad3cec69518599c944a06bd2e1d1c
designervoid <newluciocheg@gmail.com>
2019-12-19 22:28:12 +0300

    install buefy, base structure

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import HeaderView from '@/views/HeaderView'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: Home
+    path: '/header-view',
+    name: 'HeaderView',
+    component: HeaderView
   },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
 ]
 
 const router = new VueRouter({
---
+++ src/views/HeaderView.vue
@@ -0,0 +1,17 @@
+<template lang="html">
+  <HeaderComponent/>
+</template>
+
+<script>
+import HeaderComponent from '@/components/HeaderComponent.vue'
+
+export default {
+  name: 'HeaderView',
+  components: {
+    HeaderComponent
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
